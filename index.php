a48783ae060baa50619bb17955c6f34c293878dc
vadimver <vadimvereikin@gmail.com>
2016-11-04 23:19:04 +0200

    add final

--- index.php
+++ index.php
@@ -1,6 +1,2 @@
 <?php
-  $view = new \App\View();
-  $view->title = 'test site title';
-  $view->users = \App\Models\User::findAll();
-  $view->display(__DIR__ . '/App/templates/index.php');
-?>
+header('Location: controller/info.php');
